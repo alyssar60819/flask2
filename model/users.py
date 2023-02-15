9aca258c9609b59b28e6f92481e0b0798dbcf0dc
alyssar60819 <alyssar60819@gmail.com>
2023-02-15 13:42:47 -0800

    leaderboard

--- model/users.py
+++ model/users.py
@@ -16,22 +16,22 @@ class Post(db.Model):
     __tablename__ = 'posts'
 
     # Define the Notes schema
-    id = db.Column(db.Integer, primary_key=True)
-    note = db.Column(db.Text, unique=False, nullable=False)
-    image = db.Column(db.String, unique=False)
+    name = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Text, unique=False, nullable=False)
+    
     # Define a relationship in Notes Schema to userID who originates the note, many-to-one (many notes to one user)
     userID = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     # Constructor of a Notes object, initializes of instance variables within object
-    def __init__(self, id, note, image):
-        self.userID = id
-        self.note = note
-        self.image = image
+    def __init__(name, score):
+        self.name = name
+        self.score = score
+      
 
     # Returns a string representation of the Notes object, similar to java toString()
     # returns string
     def __repr__(self):
-        return "Notes(" + str(self.id) + "," + self.note + "," + str(self.userID) + ")"
+        return "Notes(" + str(self.name) + "," + str(self.score) + ")"
 
     # CRUD create, adds a new record to the Notes table
     # returns the object added or None in case of an error
@@ -56,11 +56,9 @@ class Post(db.Model):
         file_encode = base64.encodebytes(file_read)
         
         return {
-            "id": self.id,
-            "userID": self.userID,
-            "note": self.note,
-            "image": self.image,
-            "base64": str(file_encode)
+            "name": self.name,
+            "score": self.score
+        
         }
 
 
@@ -73,21 +71,18 @@ class User(db.Model):
     __tablename__ = 'users'  # table name is plural, class name is singular
 
     # Define the User schema with "vars" from object
-    id = db.Column(db.Integer, primary_key=True)
-    _name = db.Column(db.String(255), unique=False, nullable=False)
-    _uid = db.Column(db.String(255), unique=True, nullable=False)
-    _password = db.Column(db.String(255), unique=False, nullable=False)
-    _dob = db.Column(db.Date)
+    name = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.String(255), unique=False, nullable=False)
+
 
     # Defines a relationship between User record and Notes table, one-to-many (one user to many notes)
     posts = db.relationship("Post", cascade='all, delete', backref='users', lazy=True)
 
     # constructor of a User object, initializes the instance variables within object (self)
-    def __init__(self, name, uid, password="123qwerty", dob=date.today()):
+    def __init__(self, name, score):
         self._name = name    # variables with self prefix become part of the object, 
-        self._uid = uid
-        self.set_password(password)
-        self._dob = dob
+        self._score = score
+       
 
     # a name getter method, extracts name from object
     @property
@@ -101,48 +96,22 @@ class User(db.Model):
     
     # a getter method, extracts email from object
     @property
-    def uid(self):
-        return self._uid
+    def score(self):
+        return self.score
     
     # a setter function, allows name to be updated after initial object creation
     @uid.setter
-    def uid(self, uid):
-        self._uid = uid
+    def score(self, score):
+        self._score = score
         
     # check if uid parameter matches user id in object, return boolean
-    def is_uid(self, uid):
-        return self._uid == uid
+    def is_uid(self, score):
+        return self._score == score
     
-    @property
-    def password(self):
-        return self._password[0:10] + "..." # because of security only show 1st characters
-
-    # update password, this is conventional setter
-    def set_password(self, password):
-        """Create a hashed password."""
-        self._password = generate_password_hash(password, method='sha256')
-
-    # check password parameter versus stored/encrypted password
-    def is_password(self, password):
-        """Check against hashed password."""
-        result = check_password_hash(self._password, password)
-        return result
+    
     
     # dob property is returned as string, to avoid unfriendly outcomes
-    @property
-    def dob(self):
-        dob_string = self._dob.strftime('%m-%d-%Y')
-        return dob_string
-    
-    # dob should be have verification for type date
-    @dob.setter
-    def dob(self, dob):
-        self._dob = dob
-    
-    @property
-    def age(self):
-        today = date.today()
-        return today.year - self._dob.year - ((today.month, today.day) < (self._dob.month, self._dob.day))
+ 
     
     # output content using str(object) in human readable form, uses getter
     # output content using json dumps, this is ready for API response
@@ -165,24 +134,20 @@ class User(db.Model):
     # returns dictionary
     def read(self):
         return {
-            "id": self.id,
             "name": self.name,
-            "uid": self.uid,
-            "dob": self.dob,
-            "age": self.age,
+            "score": self.score,
             "posts": [post.read() for post in self.posts]
         }
 
     # CRUD update: updates user name, password, phone
     # returns self
-    def update(self, name="", uid="", password=""):
+    def update(self, name="", score=""):
         """only updates values with length"""
         if len(name) > 0:
             self.name = name
-        if len(uid) > 0:
-            self.uid = uid
-        if len(password) > 0:
-            self.set_password(password)
+        if len(score) > 0:
+            self.score = score
+      
         db.session.commit()
         return self
 
@@ -202,25 +167,22 @@ def initUsers():
     """Create database and tables"""
     db.create_all()
     """Tester data for table"""
-    u1 = User(name='Thomas Edison', uid='toby', password='123toby', dob=date(1847, 2, 11))
-    u2 = User(name='Nicholas Tesla', uid='niko', password='123niko')
-    u3 = User(name='Alexander Graham Bell', uid='lex', password='123lex')
-    u4 = User(name='Eli Whitney', uid='whit', password='123whit')
-    u5 = User(name='John Mortensen', uid='jm1021', dob=date(1959, 10, 21))
+    u1 = User(name = 'Thomas Edison', score = 1)
+   
 
-    users = [u1, u2, u3, u4, u5]
+    users = [u1]
 
     """Builds sample user/note(s) data"""
     for user in users:
         try:
             '''add a few 1 to 4 notes per user'''
             for num in range(randrange(1, 4)):
-                note = "#### " + user.name + " note " + str(num) + ". \n Generated by test data."
-                user.posts.append(Post(id=user.id, note=note, image='ncs_logo.png'))
+                
+                user.posts.append (Post(name = user.name, score = user.score))
             '''add user/post data to table'''
             user.create()
         except IntegrityError:
             '''fails with bad or duplicate data'''
             db.session.remove()
-            print(f"Records exist, duplicate email, or error: {user.uid}")
+            print(f"Records exist, duplicate email, or error: {user.name}")
             
